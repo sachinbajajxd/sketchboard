7113cba0415f82209d4440b081396d77b2aaf796
Sachin <sachinbajaj0001@gmail.com>
2023-10-23 01:09:09 +0530

    Add redux for toolbox

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,8 +1,14 @@
-import { useRef, useEffect } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+
+import { MENU_ITEMS } from "@/constants";
+import { actionItemClick } from '@/slice/menuSlice'
 
 const Board = () => {
 
     const canvasRef=useRef(null);
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem]);
 
     useEffect(() => {
         if(!canvasRef.current) return;
@@ -12,6 +18,8 @@ const Board = () => {
         canvas.height=window.innerHeight;
         canvas.width=window.innerWidth;
     }, []);
+
+    console.log(color, size);
 
     return (
         <canvas ref={canvasRef}></canvas>
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,15 +1,23 @@
 import styles from './index.module.css';
-import { COLORS, MENU_ITEMS } from '@/constants';
-import {useSelector} from 'react-redux';
+import { COLORS, MENU_ITEMS } from '@/constants'
+import {changeColor, changeBrushSize } from '@/slice/toolboxSlice'
+import {useDispatch, useSelector} from "react-redux";
+import cx from 'classnames';
 
 const Toolbox = () => {
 
+    const dispatch = useDispatch();
     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem]);
     const showStrokeToolOption = activeMenuItem ===MENU_ITEMS.PENCIL;
     const showBrushToolOption = activeMenuItem === MENU_ITEMS.ERASER || activeMenuItem===MENU_ITEMS.PENCIL;
 
     const updateBrushSize = (e) => {
+        dispatch(changeBrushSize({item: activeMenuItem, size: e.target.value}));
+    }
 
+    const updateColor = (newColor) => {
+        dispatch(changeColor({item: activeMenuItem, color: newColor}))
     }
 
     return (
@@ -17,12 +25,12 @@ const Toolbox = () => {
             {showStrokeToolOption && <div className={styles.toolItem}>
                 <h4 className={styles.toolText}>Stroke color</h4>
                 <div className={styles.itemContainer}>
-                    <div className={styles.colorBox} style={{backgroundColor: COLORS.BLACK}}/>
-                    <div className={styles.colorBox} style={{backgroundColor: COLORS.RED}}/>
-                    <div className={styles.colorBox} style={{backgroundColor: COLORS.ORANGE}}/>
-                    <div className={styles.colorBox} style={{backgroundColor: COLORS.YELLOW}}/>
-                    <div className={styles.colorBox} style={{backgroundColor: COLORS.BLUE}}/>
-                    <div className={styles.colorBox} style={{backgroundColor: COLORS.GREEN}}/>
+                    <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.BLACK})} style={{backgroundColor: COLORS.BLACK}} onClick={() => updateColor(COLORS.BLACK)}/>
+                    <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.RED})} style={{backgroundColor: COLORS.RED}} onClick={() => updateColor(COLORS.RED)} />
+                    <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.ORANGE})} style={{backgroundColor: COLORS.ORANGE}} onClick={() => updateColor(COLORS.ORANGE)} />
+                    <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.YELLOW})} style={{backgroundColor: COLORS.YELLOW}} onClick={() => updateColor(COLORS.YELLOW)} />
+                    <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.BLUE})} style={{backgroundColor: COLORS.BLUE}} onClick={() => updateColor(COLORS.BLUE)} />
+                    <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.GREEN})} style={{backgroundColor: COLORS.GREEN}} onClick={() => updateColor(COLORS.GREEN)} />
                     {/* colors */}
                 </div>
             </div>}
