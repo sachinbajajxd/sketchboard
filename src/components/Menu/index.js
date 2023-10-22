d15ee6ee8e45ed5c036efeeae8adaf6e9a296238
Sachin <sachinbajaj0001@gmail.com>
2023-10-22 23:57:26 +0530

    Add Toolbox UI

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown} from '@fortawesome/free-solid-svg-icons';
 import styles from './index.module.css';
-
 const Menu = () => {
   return (
     <div className={styles.menuContainer}>
